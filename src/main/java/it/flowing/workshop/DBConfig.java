9acac5efc6ed5382f251d5d974fc5ad2a63fac85
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2020-02-18 15:24:38 +0100

    Added first SQL query

---
+++ src/main/java/it/flowing/workshop/DBConfig.java
@@ -0,0 +1,29 @@
+package it.flowing.workshop;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:database.properties")
+public class DBConfig {
+
+    @Value("${database.url}")
+    private String url;
+    @Value("${database.username}")
+    private String username;
+    @Value("${database.password}")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
--- src/main/java/it/flowing/workshop/repository/PostgreSQLUserRepository.java
+++ src/main/java/it/flowing/workshop/repository/PostgreSQLUserRepository.java
@@ -1,20 +1,74 @@
 package it.flowing.workshop.repository;
 
 
+import it.flowing.workshop.DBConfig;
 import it.flowing.workshop.model.User;
 import it.flowing.workshop.model.UserId;
 import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Component
 public class PostgreSQLUserRepository implements UserRepository {
+
+    private DBConfig dbConfig;
+
+    @Autowired
+    public PostgreSQLUserRepository(DBConfig dbConfig) {
+        this.dbConfig = dbConfig;
+    }
+
     @Override
     public List<User> list() {
-        return new ArrayList<>();
+        List<User> toReturn = new ArrayList<>();
+        Connection connection = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            connection = DriverManager.getConnection(
+                    dbConfig.getUrl(),
+                    dbConfig.getUsername(),
+                    dbConfig.getPassword()
+            );
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery("select * from users");
+            while (rs.next()) {
+                User user = new User(
+                        UserId.create(rs.getString("id")),
+                        rs.getString("name")
+                );
+
+                toReturn.add(user);
+            }
+
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) { /* ignored */}
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) { /* ignored */}
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) { /* ignored */}
+            }
+        }
+
+        return toReturn;
     }
 
     @Override
